478b881838b610f16a6f740673c51dff3c8f9a7a
leryyyyyyy <165885193+leryyyyyyy@users.noreply.github.com>
2024-10-01 14:03:04 +0800

    Delete server directory

--- server/routes/quotationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const Quotation = require("../controllers/quotation");
-
-router.post("/", Quotation.applyRateLimit, Quotation.quotation);
-
-module.exports = router;
